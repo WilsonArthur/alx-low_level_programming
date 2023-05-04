b0cc3cce1983bb89fc3b15eba8c92ddd0f760ca2
WilsonArthur <harthurwilson2016@gmail.com>
2023-05-04 06:34:05 -0700

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -6,18 +6,19 @@
  * @src: pointer source
  * Return: void
  */
+
 char *_strcat(char *dest, char *src)
 {
-	int a = -1, i;
+int a = -1, i;
+for (i = 0; dest[i] != '\0'; i++)
+;
 
-	for (i = 0; dest[i] != '\0'; i++);
-	do
-	{
+do {
 	a++;
 	dest[i] = src[a];
 	i++;
-	}
-       	while (src[a] != '\0');
-	return (dest);
+} while (src[a] != '\0');
+
+return (dest);
 }
 
